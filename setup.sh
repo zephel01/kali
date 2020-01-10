c2975e2411c6d7ce3799acf4ff2d376ebdf38d6c
zephel01 <zephel01@gmail.com>
2020-01-11 03:50:19 +0900

    修正

--- setup.sh
+++ setup.sh
@@ -12,11 +12,13 @@ apt remove fonts-vlgothic -y
 apt install fonts-vlgothic -y
 apt install ibus-anthy -y
 apt install zenity -y
+apt install locales -y
 
 #SET LANG
 sed -i -e 's/# ja_JP.UTF-8/ja_JP.UTF-8/g' /etc/locale.gen
 /usr/sbin/locale-gen
 /usr/sbin/locale-gen ja_JP.UTF-8
+/usr/sbin/update-locale LANG=ja_JP.UTF-8
 sed -i -e 's/en_US.UTF-8/ja_JP.UTF-8/g' /etc/default/locale
 echo $LANG
 /usr/bin/localectl set-keymap jp106
