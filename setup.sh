fa8159f967948b3475bc8afbe0320445da331d50
zephel01 <zephel01@gmail.com>
2020-01-11 03:06:06 +0900

    修正

--- setup.sh
+++ setup.sh
@@ -5,11 +5,11 @@ apt update
 apt upgrade
 
 #SET LANG
-sudo sed -i -e 's/# ja_JP.UTF-8/ja_JP.UTF-8/g' /etc/locale.gen
-sudo locale-gen
-sudo set-locale LANG=ja_JP.UTF-8
+sed -i -e 's/# ja_JP.UTF-8/ja_JP.UTF-8/g' /etc/locale.gen
+/usr/sbin/locale-gen
+/usr/sbin/update-locale LANG=ja_JP.UTF-8
 echo $LANG
-localectl set-keymap jp106
+/usr/bin/localectl set-keymap jp106
 
 #jp
 apt remove fonts-vlgothi -y
