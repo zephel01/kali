bb3281d4efe4468b08636b8c9a2c8ff373ecea1e
zephel01 <zephel01@gmail.com>
2020-01-11 04:28:04 +0900

    add netadd.sh

---
+++ netadd.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cat << 'EOF' >> /etc/network/interfaces
+#primary network interface
+allow-hotplug eth0
+iface eth0 inet static
+address 10.0.0.2
+netmask 255.255.255.0
+
+#secondry network interface
+allow-hotplug eth1
+iface eth1 inet dhcp
+EOF
+
+/etc/init.d/networking stop
+/etc/init.d/networking start
