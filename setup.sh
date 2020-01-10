84feb6c383789dccb57c92dc880355d84709b0e3
zephel01 <zephel01@gmail.com>
2020-01-11 02:24:22 +0900

    ch LANG ja_JP.UTF-8

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ apt upgrade
 #SET LANG
 sudo sed -i -e 's/# ja_JP.UTF-8/ja_JP.UTF-8/g' /etc/locale.gen
 sudo locale-gen
-sudo update-locale LANG=ja_JP.UTF-8
+sudo set-locale LANG=ja_JP.UTF-8
 echo $LANG
 localectl set-keymap jp106
 
