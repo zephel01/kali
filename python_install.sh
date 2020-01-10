53433cee723b1b15fab55a6abd9a92156bedd7fb
zephel01 <zephel01@gmail.com>
2020-01-11 01:07:25 +0900

    2nd commit

--- python_install.sh
+++ python_install.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-# delete us-east1.ec2.
-sudo sed -i 's/us-east-1\.ec2\.//g' /etc/apt/sources.list
-
-sed -i -e "6,9s:^:#:" ~/.bashrc
-
-# 開発環境インストール
-sudo apt-get update
-sudo apt upgrade
-sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev
-sudo apt-get install libffi-dev
-
 # pyenv install
 
 /usr/bin/git clone https://github.com/yyuu/pyenv.git ~/.pyenv
