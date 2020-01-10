12702c2348c9d5618c87ce046e3ec4c79bdcad43
zephel01 <zephel01@gmail.com>
2020-01-11 01:42:51 +0900

    set-keymap

--- setup.sh
+++ setup.sh
@@ -9,6 +9,7 @@ sudo sed -i -e 's/# ja_JP.UTF-8/ja_JP.UTF-8/g' /etc/locale.gen
 sudo locale-gen
 sudo update-locale LANG=ja_JP.UTF-8
 echo $LANG
+localectl set-keymap jp106
 
 #jp
 apt remove fonts-vlgothic
