045d13cb8d79c855027b354871faf6678ba27339
zephel01 <zephel01@gmail.com>
2020-01-12 07:50:04 +0900

    修正

--- setup.sh
+++ setup.sh
@@ -14,19 +14,21 @@ apt install ibus-anthy -y
 apt install zenity -y
 apt install locales -y
 apt install locales-all -y
+apt install localepurge
 
 #SET LANG & keyboad setting
 echo $LANG
+export LANG=ja_JP.UTF-8
 cp /etc/default/locale /etc/default/locale.org
 cp /etc/default/keyboard /etc/default/keyboard.org
 sed -i -e 's/pc105/jp106/g' /etc/default/keyboard
 sed -i -e 's/us/jp/g' /etc/default/keyboard
-/usr/sbin/locale-gen --purge ja_JP.UTF-8
-sed -i -e 's/# ja_JP.UTF-8/ja_JP.UTF-8/g' /etc/locale.gen
+/usr/sbin/locale-gen --purge $LANG
+sed -i -e 's/# $LANG/$LANG/g' /etc/locale.gen
 /usr/sbin/locale-gen
 /usr/sbin/dpkg-reconfigure -f noninteractive locales
-/usr/sbin/update-locale LANG=ja_JP.UTF-8
-/usr/bin/localectl set-locale LANG=ja_JP.UTF-8 LANGUAGE="ja_JP:ja"
+/usr/sbin/update-locale LANG=$LANG LC_ALL=$LANG
+/usr/bin/localectl set-locale LANG=$LANG LANGUAGE="ja_JP:ja"
 
 /usr/bin/setxkbmap jp
 /usr/sbin/service keyboard-setup restart
