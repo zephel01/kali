63339bdaa72810ccf12891665794916e1e461ba0
zephel01 <zephel01@gmail.com>
2020-01-11 17:10:27 +0900

    修正

--- setup.sh
+++ setup.sh
@@ -15,13 +15,15 @@ apt install zenity -y
 apt install locales -y
 
 #SET LANG
-sed -i -e 's/# ja_JP.UTF-8/ja_JP.UTF-8/g' /etc/locale.gen
-/usr/sbin/locale-gen
-/usr/sbin/locale-gen ja_JP.UTF-8
-/usr/sbin/update-locale LANG=ja_JP.UTF-8
-sed -i -e 's/en_US.UTF-8/ja_JP.UTF-8/g' /etc/default/locale
+#sed -i -e 's/# ja_JP.UTF-8/ja_JP.UTF-8/g' /etc/locale.gen
+#/usr/sbin/locale-gen
+#/usr/sbin/locale-gen ja_JP.UTF-8
+#/usr/sbin/update-locale LANG=ja_JP.UTF-8
+#echo $LANG
+#sed -i -e 's/en_US.UTF-8/ja_JP.UTF-8/g' /etc/default/locale
+#echo $LANG
+/usr/bin/localectl set-keymap jp106 LANG=ja_JP.UTF-8 LANGUAGE="ja_JP:ja"
 echo $LANG
-/usr/bin/localectl set-keymap jp106 LANG=ja_JP.UTF-8
 
 /usr/bin/im-config
 
