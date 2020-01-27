d0c88ce3acd7fd9919c80382e3574624374e8740
Hideki Yamamoto <h.yamamoto@bitpoint.co.jp>
2020-01-27 15:25:41 +0900

    修正

--- setup.sh
+++ setup.sh
@@ -1,32 +1,28 @@
 #!/bin/bash
 
-#update
+##update
 apt update
 apt upgrade
 
-#add package
+##add package
 apt install terminator -y
 
-#jp package
+##jp package
 apt remove fonts-vlgothic -y
 apt install fonts-vlgothic -y
 apt install ibus-anthy -y
 apt install zenity -y
 apt install locales -y
-apt install locales-all -y
-#apt install localepurge -y
 
-#SET LANG & keyboad setting
+##SET LANG & keyboad setting
 echo $LANG
 export LANG=ja_JP.UTF-8
 cp /etc/default/locale /etc/default/locale.org
 cp /etc/default/keyboard /etc/default/keyboard.org
 sed -i -e 's/pc105/jp106/g' /etc/default/keyboard
 sed -i -e 's/us/jp/g' /etc/default/keyboard
-#/usr/sbin/locale-gen --purge --no-archive
 sed -i -e 's/# $LANG/$LANG/g' /etc/locale.gen
 /usr/sbin/locale-gen
-/usr/sbin/dpkg-reconfigure -f noninteractive locales
 /usr/sbin/update-locale LANG=$LANG LC_ALL=$LANG
 /usr/bin/localectl set-locale LANG=$LANG LANGUAGE="ja_JP:ja"
 
@@ -34,17 +30,16 @@ sed -i -e 's/# $LANG/$LANG/g' /etc/locale.gen
 /usr/sbin/service keyboard-setup restart
 echo "source /etc/default/locale" >> ~/.bashrc
 source ~/.bashrc
-/usr/bin/localectl
-echo $LANG
 
+cp /etc/X11/xinit/xinputrc /etc/X11/xinit/xinputrc.org
 sed -i -e 's/default/ibus/g' /etc/X11/xinit/xinputrc
 
-#Time zone
+##Time zone
 cp /etc/localtime /etc/localtime.org
 ln -sf /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
 date
 
-#ssh
+##ssh service start
 /usr/sbin/service ssh start
 /usr/sbin/update-rc.d ssh enable
 
