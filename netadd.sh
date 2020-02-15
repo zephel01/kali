bbbb6a65154db5931ec15aebcc31b4af6c8285d5
zephel01 <zephel01@gmail.com>
2020-02-16 04:35:49 +0900

    修正

--- netadd.sh
+++ netadd.sh
@@ -4,7 +4,13 @@ kalivm=`VBoxManage list vms|awk '{print $1}'|sed "s/\"//g"`
 
 vagrant halt
 
-VBoxManage modifyvm $kalivm --nic1 nat
-VBoxManage modifyvm $kalivm --nic2 hostonly
+VBoxManage hostonlyif create 
+VBoxManage hostonlyif ipconfig vboxnet0 --ip 10.0.0.1
+VBoxManage dhcpserver add --ifname vboxnet0 --ip 10.0.0.2 --netmask 255.255.255.0 --lowerip 10.0.0.100 --upperip 10.0.0.200
+VBoxManage dhcpserver modify --ifname vboxnet0 --enable
 
-vagrant up
+VBoxManage modifyvm $kalivm --nic1 nat --nictype1 virtio
+VBoxManage modifyvm $kalivm --nic2 hostonly --nictype2 virtio --hostonlyadapter2 vboxnet0
+
+#sleep 2
+#vagrant up
